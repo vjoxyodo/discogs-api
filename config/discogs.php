9926db88bdbad198e0161f67af6a00d4d5916777
vjoxyodo <vjoxyodo@gmail.com>
2021-08-14 16:55:35 +0100

    First commit

---
+++ config/discogs.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    /** Token is your discogs token that you can get on https://www.discogs.com/settings/developers page. */
+    'token' => env('DISCOGS_TOKEN', ''),
+    'headers' => [
+        /** User-Agent is a name of your application, for example 'MyDiscogsClient/1.0 +http://mydiscogsclient.org' */
+        'User-Agent' => env('DISCOGS_USER-AGENT'),
+    ],
+];
