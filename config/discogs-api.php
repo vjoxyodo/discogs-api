e28754c6611dbc52674969fafcf52cbf52e7afd9
Pedro Lopes <pedro.lopes@future-prime.com>
2021-08-22 13:29:56 +0100

    no message

--- config/discogs-api.php
+++ config/discogs-api.php
@@ -3,7 +3,7 @@
 	return [
 		'consumer_key' 			=> env('DISCOGS_API_CONSUMER_KEY',''),
 		'consumer_shared_secret'=> env('DISCOGS_API_CONSUMER_SHARED_SECRET',''),
-	    'user_agent' 			=> env('DISCOGS_API_USER-AGENT','DISCOGS-API/1 +https://github.com/vjoxyodo/discogs'),
+	    'user_agent' 			=> env('DISCOGS_API_USER_AGENT','DISCOGS-API/1 +https://github.com/vjoxyodo/discogs'),
 	    'oauth_verifier'		=> env('DISCOGS_API_OAUTH_VERIFIER',''),
 	    'oauth_token'			=> env('DISCOGS_API_OAUTH_TOKEN',''),
 	    'access_token'			=> env('DISCOGS_API_ACCESS_TOKEN',''),
