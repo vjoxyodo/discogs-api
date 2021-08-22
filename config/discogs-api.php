7922c624cb9c6825458c80c5e22422a44f2f02c6
Pedro Lopes <pedro.lopes@future-prime.com>
2021-08-22 13:29:25 +0100

    no message

--- config/discogs-api.php
+++ config/discogs-api.php
@@ -1,11 +1,11 @@
 <?php
 
 	return [
-		'consumer_key' 			=> env('DISCOGS-API_CONSUMER_KEY',''),
-		'consumer_shared_secret'=> env('DISCOGS-API_CONSUMER_SHARED_SECRET',''),
-	    'user_agent' 			=> env('DISCOGS-API_USER-AGENT','DISCOGS-API/1 +https://github.com/vjoxyodo/discogs'),
-	    'oauth_verifier'		=> env('DISCOGS-API_OAUTH_VERIFIER',''),
-	    'oauth_token'			=> env('DISCOGS-API_OAUTH_TOKEN',''),
-	    'access_token'			=> env('DISCOGS-API_ACCESS_TOKEN',''),
-	    'access_token_secret'	=> env('DISCOGS-API_ACCESS_TOKEN_SECRET','')
+		'consumer_key' 			=> env('DISCOGS_API_CONSUMER_KEY',''),
+		'consumer_shared_secret'=> env('DISCOGS_API_CONSUMER_SHARED_SECRET',''),
+	    'user_agent' 			=> env('DISCOGS_API_USER-AGENT','DISCOGS-API/1 +https://github.com/vjoxyodo/discogs'),
+	    'oauth_verifier'		=> env('DISCOGS_API_OAUTH_VERIFIER',''),
+	    'oauth_token'			=> env('DISCOGS_API_OAUTH_TOKEN',''),
+	    'access_token'			=> env('DISCOGS_API_ACCESS_TOKEN',''),
+	    'access_token_secret'	=> env('DISCOGS_API_ACCESS_TOKEN_SECRET','')
 	];
