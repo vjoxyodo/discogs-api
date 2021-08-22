6003cf878468e3bebe94af28a0c46a0ab96325d4
Pedro Lopes <pedro.lopes@future-prime.com>
2021-08-22 13:19:57 +0100

    no message

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # discogs
 Package for use of www.discogs.com API
+
+
+
+#php artisan vendor:publish --provider="Vjoxyodo\DiscogsApi\DiscogsServiceProvider"
--- config/discogs-api.php
+++ config/discogs-api.php
@@ -1,8 +1,8 @@
 <?php
 
 	return [
-		'consumer_key' 			=> env('DISCOGS_CONSUMER_KEY','BievXGiGrBvlwbyqnSmF'),
-		'consumer_shared_secret'=> env('DISCOGS_CONSUMER_SHARED_SECRET','pGsEyOJkAgsPwCiDpaTbxRpUsVyqSgEI'),
-		'access_token_secret'	=> env('DISCOGS_ACCESS_TOKEN_SECRET','bhJQWnAsRCpiFQddcSSCukSLraHFHwHKqYwCSwrs'),
-	    'user_agent' 			=> env('DISCOGS_USER-AGENT','user-agent')
+		'consumer_key' 			=> env('DISCOGS_CONSUMER_KEY',''),
+		'consumer_shared_secret'=> env('DISCOGS_CONSUMER_SHARED_SECRET',''),
+		'access_token_secret'	=> env('DISCOGS_ACCESS_TOKEN_SECRET',''),
+	    'user_agent' 			=> env('DISCOGS_USER-AGENT','')
 	];
