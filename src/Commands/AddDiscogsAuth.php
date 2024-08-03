1772cb0178eb76a23e7fbd8bf073f22c9a2d2719
Pedro Lopes <pedro.lopes@inboxagency.com>
2024-08-03 18:28:47 +0100

    - Added command to question about API V2 token insert into Laravel project .env file
    Version: 0.5.0.1

--- src/Commands/AddDiscogsAuth.php
+++ src/Commands/AddDiscogsAuth.php
@@ -30,7 +30,28 @@ class AddDiscogsAuth extends Command
      */
     public function handle(): void
     {
-	    $this->info("Testing Command");
+	    
+		// Initialize variable to store the user's name
+        $DISCOGS_API_ACCESS_TOKEN = '';
+
+        // Loop until a valid string is entered
+        while (true) {
+		    $DISCOGS_API_ACCESS_TOKEN = $this->ask('Please insert your app token?');
+
+            // Check if the input is a valid string
+            if (strlen($DISCOGS_API_ACCESS_TOKEN) == 40) {
+	            putenv('DISCOGS_API_ACCESS_TOKEN='$DISCOGS_API_ACCESS_TOKEN);
+	            $this->info("Your token {$DISCOGS_API_ACCESS_TOKEN} has been added to your project .env with success!");
+	            $this->info("Support here https://github.com/vjoxyodo/discogs-api/")
+	            $this->
+	            
+                break;
+            } else {
+                $this->error('Please enter a valid 40 chars Discogs API v2.');
+            }
+        }
+        
+        return Command::SUCCESS;
 	    
     }
 }
