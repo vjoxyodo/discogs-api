627d951af81f0cfbe5fada00fbb494a8724acca9
Pedro Lopes <pedro.lopes@inboxagency.com>
2024-08-03 18:34:34 +0100

    no message

--- src/Commands/AddDiscogsAuth.php
+++ src/Commands/AddDiscogsAuth.php
@@ -29,9 +29,7 @@ class AddDiscogsAuth extends Command
             if (strlen($DISCOGS_API_ACCESS_TOKEN) == 40) {
 	            putenv('DISCOGS_API_ACCESS_TOKEN='$DISCOGS_API_ACCESS_TOKEN);
 	            $this->info("Your token {$DISCOGS_API_ACCESS_TOKEN} has been added to your project .env with success!");
-	            $this->info("Support here https://github.com/vjoxyodo/discogs-api/")
-	            $this->
-	            
+	            $this->info("Support here https://github.com/vjoxyodo/discogs-api/");	            
                 break;
             } else {
                 $this->error('Please enter a valid 40 chars Discogs API v2.');
