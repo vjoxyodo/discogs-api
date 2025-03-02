f672e865833bd684e194705a79ee3a068b1ee8b8
Pedro Lopes <pedro.lopes@inboxagency.com>
2025-03-02 14:02:33 +0000

    no message

--- src/Commands/AddDiscogsAuth.php
+++ src/Commands/AddDiscogsAuth.php
@@ -9,6 +9,7 @@ class AddDiscogsAuth extends Command
 
     protected $signature = 'discogsapi:userdata';
  
+	protected $envConstant = 'DISCOGS_API_ACCESS_TOKEN';
  
 	public function __construct()
 	{
@@ -24,16 +25,67 @@ class AddDiscogsAuth extends Command
         $DISCOGS_API_ACCESS_TOKEN = '';
 
         while (true) {
-		    $DISCOGS_API_ACCESS_TOKEN = $this->ask('Please insert your app token?');
+	        $DISCOGS_API_ACCESS_TOKEN = $this->ask('Please insert your app token?');
+		    
 
-            if (strlen($DISCOGS_API_ACCESS_TOKEN) == 40) {
-	            putenv('DISCOGS_API_ACCESS_TOKEN=' . $DISCOGS_API_ACCESS_TOKEN);
-	            $this->info("Your token " . $DISCOGS_API_ACCESS_TOKEN . " has been added to your project .env with success!");
-	            $this->info("Support here https://github.com/vjoxyodo/discogs-api/");	            
+            if (strlen($DISCOGS_API_ACCESS_TOKEN) > 1 ){ #== 40) {
+   
+				$file = base_path('.env');
+				
+				$this->ask("<options=bold;fg=white;bg=red>Your .env file doesn't exist!</> Do you want to create it? (yes or y) ");
+
+					exit();
+				if(!file_exists($file)){
+
+				}
+
+				$rawData = file_get_contents($file);
+				
+				$data = explode("\n",$rawData);
+				
+				$findCheck = array_filter($data, function($value) {
+				    return strpos($value, $this->envConstant . "=") !== false;
+				});
+				
+				$answer = "no";
+				
+					
+				if(!empty($findCheck)){
+					$findKey = key($findCheck);
+					
+					$answer = $this->ask('You already have this env variable, are you sure you want to replace it? (yes or y)');
+					
+					if (in_array($answer, ["yes", "y"])){
+						$data[$findKey] = $this->envConstant . "=" . $DISCOGS_API_ACCESS_TOKEN;
+					}
+					
+				}else{
+					array_push($data, $this->envConstant . "=" . $DISCOGS_API_ACCESS_TOKEN);
+				}
+				
+				if (file_exists($file)) {
+					if(in_array($answer, ["yes", "y"])){
+						file_put_contents($file, implode("\n", $data));
+						$this->info("Your token, <options=bold;fg=cyan;bg=black>" . $DISCOGS_API_ACCESS_TOKEN . "</> has been added to your project .env with success!");
+					}
+				}else{
+					$this->error("Something went wrong adding your token!");
+				}
+
+				$this->support();
+
                 break;
             } else {
                 $this->error('Please enter a valid 40 chars Discogs API v2.');
             }
+
         }  
+    }
+    
+    public function support(){
+	    $this->info("");
+	    $this->info("Support here <bg=blue;fg=green;options=underscore>https://github.com/vjoxyodo/discogs-api/</>");
+	    $this->info("🔊 You can check my record collection <bg=blue;fg=green;options=underscore>https://www.discogs.com/user/vjoxyodo/collection</>, drop a message!");
+	    $this->info("");
     }
 }
